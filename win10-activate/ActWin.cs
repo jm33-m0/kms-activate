7fdc5277657970cf850ba345c324752a4dcc7a5c
jm33-m0 <jm33@jm33.me>
2025-09-26 20:34:17 +1000

    support win2k25

--- win10-activate/ActWin.cs
+++ win10-activate/ActWin.cs
@@ -25,6 +25,8 @@ namespace kms_activate
                 {"Windows 7 Enterprise", "33PXH-7Y6KF-2VJC9-XBBR8-HVTHH" },
                 {"Windows Server 2022 Standard", "VDYBN-27WPP-V4HQT-9VMD4-VMK7H" },
                 {"Windows Server 2022 Datacenter", "WX4NM-KYWYW-QJJR4-XV3QB-6VM33" },
+                {"Windows Server 2025 Standard", "TVRH6-WHNXV-R9WG3-9XRFY-MY832" },
+                {"Windows Server 2025 Datacenter", "D764K-2NDRG-47T6Q-P8T8W-YP6DF" },
                 {"Windows Server 2019 Standard", "N69G4-B89J2-4G8F4-WWYCC-J464C" },
                 {"Windows Server 2019 Datacenter", "WMDGN-G9PQG-XVVXX-R3X43-63DFG" },
                 {"Windows Server 2016 Standard", "WC2BQ-8NRM3-FDDYY-2BFGV-KHKQY" },
